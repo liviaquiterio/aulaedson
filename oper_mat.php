6bac4cd37df5d0465dd5cdc6179af360c77b5a83
liviaquiterio <127116102+liviaquiterio@users.noreply.github.com>
2024-02-27 09:33:31 -0300

    Add files via upload

---
+++ oper_mat.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio</title>
+</head>
+<body>
+<?php
+    echo "SOMA";
+    $numero1=20; 
+    echo "<hr>";
+    $numero2=20;
+    echo "<hr>";
+    $soma = $numero1 + $numero2;
+    echo $numero1 + $numero2;
+    echo "<br>Total: " . $soma;
+
+    echo "<br>";
+    echo "Subtração";
+    $numero3=13; 
+    echo "<hr>";
+    $numero4=6;
+    echo "<hr>";
+    "<br>";
+    $subtracao = $numero3 - $numero4;
+    echo $numero3 - $numero4;
+    echo "<br>Total: " . $subtracao;
+
+    echo "<br>";
+
+    $numero5=5;  
+    echo "<hr>";
+    $numero6=5;
+    echo "<hr>";
+    $mult = $numero5 * $numero6;
+    echo "<br>";
+    echo "<br>Total: " . $mult;
+
+    echo "<br>";
+    $numero7=26;  
+    echo "<hr>";
+    $numero8=13;
+    echo "<hr>";
+    $div = $numero7 / $numero8;
+    echo "<br>Total: " . $div;
+    ?>
+</body>
+</html>
---
+++ variaveis1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variável tipo inteiro</title>
+</head>
+<body>
+    <h3>Variável tipo inteiro </h3>
+    <?php
+    $numero1=10; // Variável tipo inteiro
+    var_dump($numero1); // Exibe o tipo da variável
+    echo "<br>";
+    $numero2=15;
+    var_dump($numero2);
+    echo "<br>";
+    $soma = $numero1 + $numero2;
+    echo "<br>Total: " . $soma;
+    ?>
+</body>
+</html>
